6b3eb49bec46b467d9cd8e55729cca1dd3fa1ff5
Pavel Lobashov <shockwavenn@gmail.com>
2017-03-14 12:52:09 +0300

    Add support of simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
-require 'bundler/setup'
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 require 'onlyoffice_rspec_parser'
-
-RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-end
