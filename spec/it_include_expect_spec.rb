f77845f75fc66163aef5d1453f7197bf2380b0a5
Pavel Lobashov <shockwavenn@gmail.com>
2017-10-27 18:54:37 +0300

    Add pending for expect after each

--- spec/it_include_expect_spec.rb
+++ spec/it_include_expect_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe 'ItParsed#include_expect?' do
     spec = OnlyofficeRspecParser::SpecParsed.new('spec/spec_examples/rspec_parsed/expect_types_spec.rb')
     expect(spec.it_nodes.last.include_expect?).to be_truthy
   end
+
+  it 'ItParsed#include_expect? correctly true for expect in after each' do
+    pending('Parse expect in after each')
+    spec = OnlyofficeRspecParser::SpecParsed.new('spec/spec_examples/rspec_parsed/expect_after_it_spec.rb')
+    expect(spec.it_nodes.last.include_expect?).to be_truthy
+  end
 end
